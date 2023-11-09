6b455c20606abd840b0213fd2dd63b19cf2cddf4
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-10 00:16:17 +0700

    add page: counter

---
+++ src/views/CounterView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0)
+</script>
+<template>
+    <div class="counter">
+        <button @click="count++">
+            {{ count }}
+        </button>
+    </div>
+</template>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
