f658a1374b5bbe060bb7cdb0f317c78d009d853d
Agisna Revaldo <agisrifaldo75@gmail.com>
2023-11-10 23:12:53 +0700

    just commit

--- src/views/CounterView.vue
+++ src/views/CounterView.vue
@@ -5,17 +5,21 @@ const count = ref(0)
 </script>
 <template>
     <div class="counter">
-        <button @click="count++">
-            {{ count }}
-        </button>
+        <button @click="count++">+</button>
+        <h1>{{ count }}</h1>
+        <button @click="count--">-</button>
     </div>
 </template>
 <style>
 @media (min-width: 1024px) {
-  .about {
+  .counter {
     min-height: 100vh;
     display: flex;
     align-items: center;
+  }
+  button {
+    font-weight: bold;
+    padding: 5px 10px;
   }
 }
 </style>
